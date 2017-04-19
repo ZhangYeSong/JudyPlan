d5b132cf0dc77ab0c93a7661f8cf4ae2d02feebf
Judy19920322 <17602105720@163.com>
2017-04-19 23:25:28 +0800

    list布局书写

--- app/src/main/java/com/song/judyplan/ListActivity.java
+++ app/src/main/java/com/song/judyplan/ListActivity.java
@@ -1,13 +1,19 @@
 package com.song.judyplan;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class ListActivity extends AppCompatActivity {
+    private RecyclerView mRvList;
+    private FloatingActionButton mFabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        mRvList = (RecyclerView) findViewById(R.id.rv_list);
+        mFabAdd = (FloatingActionButton) findViewById(R.id.fab_add);
     }
 }
