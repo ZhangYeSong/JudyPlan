7bd043f970f287380b60e1bc99661d5689a560c4
Judy19920322 <522483446@qq.com>
2017-04-19 23:14:26 +0800

    建立CalendarActivity, ListActivity和之间的跳转

--- app/src/main/java/com/song/judyplan/CalendarActivity.java
+++ app/src/main/java/com/song/judyplan/CalendarActivity.java
@@ -1,13 +1,31 @@
 package com.song.judyplan;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
-public class CalendarActivity extends AppCompatActivity {
+/**
+ *  An activity show calendar
+ *  Create by song at 2017.4.19
+ */
+public class CalendarActivity extends AppCompatActivity implements CalendarView.OnDateChangeListener {
+    private CalendarView mCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        mCalendar = (CalendarView) findViewById(R.id.calendar);
+        mCalendar.setOnDateChangeListener(this);
+    }
+
+    @Override
+    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+        Toast.makeText(getApplicationContext(), year + "年" + (month + 1) + "月" + dayOfMonth + "日",
+                Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, ListActivity.class));
     }
 }
